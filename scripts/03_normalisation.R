c5818cdbe11e60e10ba26cc5f71b7a284322420f
SimonVandersanden123 <simon.vandersanden@uhasselt.be>
2026-03-09 15:34:34 +0100

    added some normalisation methods, GMB

--- scripts/03_normalisation.R
+++ scripts/03_normalisation.R
@@ -2,15 +2,31 @@
 #Handling the "Zero Problem" and Compositional Transformations
 
 library(microbiome) # Excellent for CLR and TSS functions
+library(zCompositions)
 
 # 1. Handling Sparsity (The Zero Problem)
 # ---------------------------------------
-# For CLR, we must replace zeros. Here we add a pseudo-count of 1.
-# This shifts the data into a space where log(x) is defined.
-
+# Method 1: Simple Pseudo-count
+# Logic: Adds a constant (1) to all cells. Fast but can bias ratios.
 mibi_pseudo <- transform(mibi_phylo_object_filt, transform = "shift", shift = 1)
+######################################################################################
+# Method 2: Bayesian-Multiplicative Replacement (BMR)
+# Logic: Imputes zeros based on a Dirichlet prior and shifts non-zeros multiplicatively.
+# Note: cmultRepl works on a samples-as-rows, taxa-as-columns matrix.
+otu_table_matrix <- as.matrix(abundances(mibi_phylo_object_filt))
+# Transpose because cmultRepl expects samples in rows
+otu_mat_t <- t(otu_table_matrix)
+# Perform BMR (Geometric Bayesian Multiplicative method)
+# be carefull for singletons, it wont work with singletons
+# output = "p-counts" returns pseudo-counts that preserve ratios
+bmr_counts <- cmultRepl(otu_mat_t, method = "GBM", output = "p-counts")
 
-#Add a part when proper pseudocounting, using Bayesian-Multiplicative Replacement
+# Re-integrate into a phyloseq object
+mibi_bmr <- mibi_phylo_object_filt
+otu_table(mibi_bmr) <- otu_table(t(bmr_counts), taxa_are_rows = TRUE)
+######################################################################################
+# Method 3: Matrix completion (MC)
+
 
 # 2. Transformation Methods
 # ---------------------------------------
@@ -22,8 +38,11 @@ mibi_tss <- transform(mibi_phylo_object_filt, transform = "compositional")
 # Method B: Centered Log-Ratio (CLR)
 # Logic: log(x_i / geometric_mean(x))
 # This moves data from the "simplex" (bounded 0-1) to Euclidean space.
-mibi_clr <- transform(mibi_pseudo, transform = "clr")
+mibi_pseudo_clr <- transform(mibi_pseudo, transform = "clr")
 
+mibi_bmr_clr <- transform(mibi_bmr, transform = "clr")
+
+mibi_mc_clr <- transform(mibi_mc, transform = "clr")
 # 3. Validation
 # ---------------------------------------
 message("Normalization complete:")
